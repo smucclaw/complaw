53a8fb45744e2d9917b64d8f1d831dc51dff8465
Teo Wei Shen <tws4793@gmail.com>
2021-03-03 16:27:04 +0800

    Rename to exempi

--- doc/ex-xmp-20200808/xmp/xmp_pdf/exempi.py
+++ doc/ex-xmp-20200808/xmp/xmp_pdf/exempi.py
@@ -1,37 +1,37 @@
 import json
+import xmltodict
 from libxmp import XMPFiles, consts
 
-def read_xmp(file_pdf = ''):
-    xmp_file = XMPFiles(file_path = file_pdf, open_forupdate = True)
+def read_xmp(file = ''):
+    xmp_file = XMPFiles(file_path = file, open_forupdate = True)
 
     # Extract XMP
     xmp = xmp_file.get_xmp()
+    json = xmltodict.parse(xmp)
 
-    return xmp
+    out = json.dumps(json)
+    return out
 
-def write_xmp(file_pdf = '', file_json = ''):
+def write_xmp(pdf = '', json = ''):
     '''
     Opens an PDF file and JSON file for writing JSON into XMP.
 
     Parameters
-        file_pdf (str) : Location of the PDF file
-        file_json (str) : Location of the JSON file
+        pdf (str) : Location of the PDF file
+        json (str) : Location of the JSON file
     '''
-    xmp_file = XMPFiles(file_path = file_pdf, open_forupdate = True)
+    xmp_file = XMPFiles(file_path = pdf, open_forupdate = True)
 
     # Extract XMP
     xmp = xmp_file.get_xmp()
 
     # Insert XMP
-    xmp = insert_xmp(xmp, file_json)
+    xmp = insert_xmp(xmp, json)
 
     print(xmp)
     # Put into file
     xmp_file.put_xmp(xmp)
     xmp_file.close_file()
-
-def get_json(xmp):
-    pass
 
 def insert_xmp(xmp, file_json, encode = consts.XMP_NS_PDFX):
     '''
@@ -63,5 +63,5 @@ def insert_xmp(xmp, file_json, encode = consts.XMP_NS_PDFX):
     
     return xmp
 
-if __name__ == '__main__':
-    write_xmp(file_pdf = 'docs/gdoc4.pdf', file_json = 'docs/alt2.json') 
+# if __name__ == '__main__':
+#     write_xmp(file_pdf = 'docs/gdoc4.pdf', file_json = 'docs/alt2.json') 
