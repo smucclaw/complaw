c006d91221a0bb31d146cf092e1dc1882cbf5755
Teo Wei Shen <tws4793@gmail.com>
2021-03-30 22:11:28 +0800

    make changes to process code

--- doc/ex-xmp-20200808/xmp/xmp_pdf/process.py
+++ doc/ex-xmp-20200808/xmp/xmp_pdf/process.py
@@ -11,10 +11,10 @@ def process(args):
     elif args.silent:
         pass
     
-    with exif.ExifTool() as e:
+    with exif.MetaTool(prefix = 'L4') as e:
         is_read = args.mode == 0
         if is_read:
-            return read(e, args)
+            return read2(e, args)
         else:
             return write(e, args)
 
@@ -37,21 +37,46 @@ def read(e, args):
     
     return result
 
-def write(e, args):
-    # Copy over file
-    shutil.copy2(args.input.name, args.output.name)
+def read2(e, args):
+    meta = e.read(args.file[0].name)
 
-    # Detect metadata file type
+    if args.json or args.type == 'json':
+        result = json.dumps(meta, indent = 4)
+    elif args.yaml or args.type == 'yaml':
+        result = yamp.dumps(meta)
+
+    return result
+
+def write(e, args):
+    result = ''
+
+    # Copy over file
+    shutil.copy2(args.input[0].name, args.output[0].name)
+
+    # Process metafile
     metafile = args.meta[0].name
-    metafile_name, metafile_ext = os.path.splitext(metafile)
-    
-    # Depending on the type of file, parse
+
     with open(metafile) as file:
+        # Detect metadata file type
+        metafile_ext = detect_file_type(metafile)
+
         if metafile_ext == '.json':
+            new_file = 'new2.json'
+
             metadata = json.load(file)
+            meta_flat = e.stringify(metadata)
+            convert_to_flat(meta_flat, new_file)
+            
+            result = e.write(args.output[0].name, metafile = new_file)
         elif metafile_ext == '.yml' or metafile_ext == '.yaml':
             metadata = yaml.load(file, Loader = yaml.FullLoader)
 
-    result = ''
-
     return result
+
+def detect_file_type(filename):
+    name, ext = os.path.splitext(filename)
+    return ext
+
+def convert_to_flat(meta, new_file):
+    with open(new_file, 'w+') as file:
+        file.write(meta + "\n")
