7d9054a1ff642381b713485ff550ab8f6394c558
Teo Wei Shen <tws4793@gmail.com>
2021-04-24 12:05:08 +0800

    make code pep8 compliant

--- doc/ex-xmp-20200808/xmp/l4meta/terminal.py
+++ doc/ex-xmp-20200808/xmp/l4meta/terminal.py
@@ -1,6 +1,7 @@
 import argparse
 import sys
 
+
 def arguments():
     '''
     Prepare the argument list.
@@ -11,16 +12,14 @@ def arguments():
     props = {
         'prog': 'l4meta',
         'description': 'Read/Write L4 metadata',
-        'allow_abbrev': False
-    }
-    
+        'allow_abbrev': False}
     parser = argparse.ArgumentParser(**props)
-
     subparser = parser.add_subparsers()
     read(subparser)
     write(subparser)
 
     return parser
+
 
 def read(subparser):
     '''
@@ -29,76 +28,66 @@ def read(subparser):
     Returns:
         parser (ArgumentParser): parser
     '''
-    
     parser = subparser.add_parser(
         'read',
-        help = 'Read XMP from PDF into stdout'
-    )
+        help='Read XMP from PDF into stdout')
 
     parser.add_argument(
         'is_read',
-        action = 'store_true',
-        help = argparse.SUPPRESS
-    )
+        action='store_true',
+        help=argparse.SUPPRESS)
 
     parser.add_argument(
         'file',
-        help = 'location of file',
-        type = str,
-        nargs = 1
-    )
+        help='location of file',
+        type=str,
+        nargs=1)
 
     outputs = parser.add_mutually_exclusive_group()
-    
     outputs.add_argument(
         '-t', '--type',
-        help = 'specify metadata output format',
-        choices = ['json', 'yaml'],
-        default = 'json'
-    )
+        help='specify metadata output format',
+        choices=['json', 'yaml'],
+        default='json')
     outputs.add_argument(
         '-j', '--json',
-        help = 'output metadata in JSON, same as --type json',
-        action = 'store_const',
-        dest = 'type',
-        const = 'json'
-    )
+        help='output metadata in JSON, same as --type json',
+        action='store_const',
+        dest='type',
+        const='json')
     outputs.add_argument(
         '-y', '--yaml',
-        help = 'output metadata in YAML, same as --type yaml',
-        action = 'store_const',
-        dest = 'type',
-        const = 'yaml'
-    )
+        help='output metadata in YAML, same as --type yaml',
+        action='store_const',
+        dest='type',
+        const='yaml')
+
 
 def write(subparser):
+    '''
+    '''
     parser = subparser.add_parser(
         'write',
-        help = 'Write XMP to PDF from JSON'
-    )
-    
+        help='Write XMP to PDF from JSON')
+
     parser.add_argument(
         'is_read',
-        action = 'store_false',
-        help = argparse.SUPPRESS
-    )
-    
+        action='store_false',
+        help=argparse.SUPPRESS)
+
     parser.add_argument(
         'input',
-        help = 'location of PDF to be read',
-        type = str,
-        nargs = 1
-    )
+        help='location of PDF to be read',
+        type=str,
+        nargs=1)
     parser.add_argument(
         'output',
-        help = 'location of PDF to be written',
-        type = str,
-        nargs = 1
-    )
+        help='location of PDF to be written',
+        type=str,
+        nargs=1)
     parser.add_argument(
         'meta',
-        help = 'location of metadata',
-        type = argparse.FileType('r', encoding = 'UTF-8'),
-        nargs = '?',
-        default = sys.stdin
-    )
+        help='location of metadata',
+        type=argparse.FileType('r', encoding='UTF-8'),
+        nargs='?',
+        default=sys.stdin)
